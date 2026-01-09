6ae25bdd3b26dfad993cf85ba7461f4777c54678
Lucas C <lucas.sn.chow@gmail.com>
2026-01-09 18:42:03 -0500

    Time: 39 ms (43.91%) | Memory: 12.9 MB (38.80%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,14 +1,26 @@
 class Solution(object):
     def checkInclusion(self, s1, s2):
         # Sort s1 once
-        target = "".join(sorted(s1))
-        n = len(s1)
-        
-        # Slide a window of size n across s2
-        for i in range(len(s2) - n + 1):
-            # Extract the substring of length n and sort it
-            current_slice = "".join(sorted(s2[i : i + n]))
-            if current_slice == target:
+        l1,l2 = len(s1),len(s2)
+        fq_count = Counter(s1)
+        window_count= Counter(s2[:len(s1)])
+        if fq_count == window_count:
+            return True
+
+        for i in range(l1,l2):
+            old_char = s2[i-l1]
+            new_char = s2[i]
+
+            # remove from freq counter if 1 and is old char 
+            if window_count[old_char] == 1:
+                del window_count[old_char]
+            else:
+                window_count[old_char] -= 1
+            
+            # new char 
+            window_count[new_char] = 1 + window_count.get(new_char,0)
+            if window_count == fq_count:
                 return True
-                
+
+
         return False
