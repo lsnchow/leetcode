20dcbf987ae94ea1174c6d65ea4f3c4c04461353
Lucas C <lucas.sn.chow@gmail.com>
2026-01-11 17:08:12 -0500

    Time: 620 ms (5.01%) | Memory: 15.7 MB (22.02%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,21 +1,27 @@
 class Solution(object):
     def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
         r = 0
         l = 0
+        d = {}
         m = 0
-        mf = {}
-        maxf = 0
-        for r in range(len(s)):
-            mf[s[r]] = 1 + mf.get(s[r],0)
-            
-            maxf = max(mf[s[r]],maxf)
-            while (r - l + 1) - maxf > k:
-                mf[s[l]] -= 1
-                l += 1
-            
-            # Update the result with the largest valid window found
-            m = max(m, r - l + 1)
+        left = 0
+        n = len(s)
+        for right in range(n):
+            d[s[right]] = 1 + d.get(s[right],0)
+            mfc = sorted([d[key],key] for key in d)[-1][0]
+            while right-left+1-mfc > k:
+                # remove
+                if d[s[left]] == 1:
+                    del d[s[left]]
+                else:
+                    d[s[left]] -= 1
+                left += 1
+            m = max(m,right-left+1)  
         return m
 
                 
-            
